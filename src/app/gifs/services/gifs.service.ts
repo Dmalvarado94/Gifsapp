dae2f69853652aea5da65c6b3d3a10298f98d1f3
Daniel Moya <dmalvarado94@gmail.com>
2022-05-27 18:06:39 -0400

    Realizar peticion HTTP externo de gifs con postman

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,11 +6,17 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
+  private apiKey : string = 'J2JrToRSC2453VL1H5AJb19qhNGiy59P'
+
   private _historial: string[] = [];
 
   get historial () {
     
     return [...this._historial];
+  }
+
+  constructor( private http: HttpClient) {
+
   }
 
   buscarGifs( query: string = '' ) {
@@ -21,9 +28,13 @@ export class GifsService {
       this._historial = this.historial.splice(0,10);
   }
 
-    console.log(this._historial);
+  this.http.get('https://api.giphy.com/v1/gifs/search?api_key=J2JrToRSC2453VL1H5AJb19qhNGiy59P&limit=15&q=')
+      .subscribe ( ( resp: any ) => {
+        console.log(resp.data);
+      });
+  
+    }
+
 
   }
 
-
-}
