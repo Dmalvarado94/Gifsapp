e20ccaf8b6ba08423d78459b9e3994562f8d716c
Daniel Moya <dmalvarado94@gmail.com>
2022-05-30 18:48:25 -0400

    recordando el ultimo historial

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class GifsService {
 
   private _historial: string[] = [];
 
-  public resultados: any[] = [];
+  public resultados: Gif[] = [];
 
   get historial () {
     
@@ -18,8 +19,20 @@ export class GifsService {
   }
 
   constructor( private http: HttpClient) {
+// existen varios modos de guardar el historial del que esta navegando en la app
+this._historial = JSON.parse ( localStorage.getItem('historial')! )  || [];
+this.resultados = JSON.parse ( localStorage.getItem('resultados')! )  || [];
 
-  }
+}
+
+
+
+// Este es el otro modo
+  //  if ( localStorage.getItem('historial') ){
+  //    this._historial = JSON.parse ( localStorage.getItem('historial')! );
+ //   }
+
+  
 
   buscarGifs( query: string = '' ) {
 
@@ -28,16 +41,20 @@ export class GifsService {
     if( !this._historial.includes( query ) ) {
       this._historial.unshift( query );
       this._historial = this.historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify( this._historial ) )  ;
   }
 
-  this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=UqX8nZ0VxB0csMrBNEDBbSAQI03vz82N&q=${ query }&limit=50`)
-      .subscribe ( (  resp: any ) => {
+  this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=UqX8nZ0VxB0csMrBNEDBbSAQI03vz82N&q=${ query }&limit=25`)
+      .subscribe ( (  resp ) => {
         console.log(resp.data);
         this.resultados = resp.data;
+        localStorage.setItem('resultados', JSON.stringify( this.resultados ) )  ;
+    
       });
   
     }
+ }
 
-
-  }
+  
 
